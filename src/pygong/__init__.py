4ee44fe32ff5156a0cebd450c0eec5a31e2fed91
gongwendong <gwd163nom@163.com>
2024-08-26 12:47:45 +0800

    [fix]module could not find version bug(#1)

--- src/pygong/__init__.py
+++ src/pygong/__init__.py
@@ -1,2 +1,3 @@
-version = open('pygong.VERSION', 'r').read().strip()
+import os
+version = open(os.path.join(os.path.dirname(os.path.abspath(__file__)),'pygong.VERSION'), 'r').read().strip()
 __version__ = version
