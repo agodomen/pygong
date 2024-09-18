ae50f5dfedbf64fdb166cd0f8c761e5aa2e0f6cc
gwdgithubnom <gwd163nom@163.com>
2024-09-18 11:19:12 +0800

    [fix]get common logger usuage(#1)

--- test/sample.py
+++ test/sample.py
@@ -9,4 +9,27 @@ Test for module dev usage
 #
 # logger.info("test")
 import pygong
+from pygong.common_log import logger
+
 print(pygong.__file__)
+print(pygong.__version__)
+log = pygong.common_log.logger
+pygong.common_log.common_logger.open_file_logger(file_location='/tmp/1.log')
+log.debug("debug")
+log.info("info")
+# log.warn("warn")
+log.warning("warning")
+log.error("error")
+# log.exception("exception")
+log.critical("critical")
+# log.fatal("fatal")
+
+# log.log(level=0,msg="log")
+# log.log(level=1,msg="log")
+# log.log(level=2,msg="log")
+# log.log(level=3,msg="log")
+# log.log(level=4,msg="log")
+# print('\033[95mThis text is close to fuchsia color.\033[0m')
+# print('\033[30;47m This text has black foreground and white background. \033[0m')
+# print('\033[30m This text has black foreground only. \033[0m')
+# print('\033[40m This text has black background only. \033[0m')
